85e46b1253ffbdb2bd33ef1a59d1dafd73c0214a
Christian Hau <ch.hau@gefro.de>
2023-03-05 16:02:44 +0100

    Use archie-metadata in script

--- archive-add.sh
+++ archive-add.sh
@@ -6,6 +6,7 @@
 
 BASE=$(realpath "$(dirname "$0")")
 ARCHIVE_COMMAND="$BASE/bin/archive"
+METADATA_COMMAND="$BASE/bin/archive-metadata"
 
 source="$1"
 if [ "$source" == "" ]; then
@@ -13,14 +14,36 @@ if [ "$source" == "" ]; then
   exit
 fi
 
+### Functions ###
+
+function add_to_archive() {
+  file="$1"
+  if [ ! -f "$file" ]; then
+    echo "File not found: $file"
+    return
+  fi
+
+  hash=$($ARCHIVE_COMMAND "$file" -s)
+  if [ "$hash" != "FAIL" ]; then
+    $METADATA_COMMAND "$hash" "add-origin" "$file"
+  else
+    echo "Failed to add file ($hash)"
+  fi
+}
+
+### Main ###
+
 if [ -d "$source" ]; then
+
   old_ifs=$IFS
   IFS=$'\n'
   for f in $(find "$source" -type f); do
-    $ARCHIVE_COMMAND "$f"
+    add_to_archive "$f"
   done
   IFS=$old_ifs
 
-elif [ -f "$source" ]; then
-  $ARCHIVE_COMMAND "$source"
+else
+
+  add_to_archive "$source"
+
 fi
